c8693128a49e42d3b4d87722cd70d8d2a92a6fb7
BerkYeni <141870638+BerkYeni@users.noreply.github.com>
2023-09-26 17:18:52 +0300

    wrote/rewrote tests for isValidWindowHours.

--- src/tests/utilities.test.tsx
+++ src/tests/utilities.test.tsx
@@ -1,54 +1,729 @@
 import { expect, test } from "vitest";
-import { HourWindow } from "../interfaces";
 import { isValidHourWindows } from "../utilities";
 
 test("isValidHourWindows()", () => {
-  const windowA: HourWindow = {
-    start: { hours: 10, minutes: 0 },
-    end: { hours: 11, minutes: 30 },
-  };
+  // single
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 13, minutes: 0 },
+        end: { hours: 15, minutes: 0 },
+      },
+    ])
+  ).toBe(true);
 
-  expect(isValidHourWindows([windowA])).toBe(true);
+  // same time
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 0, minutes: 0 },
+        end: { hours: 0, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
 
-  const windowB: HourWindow = {
-    start: { hours: 14, minutes: 30 },
-    end: { hours: 17, minutes: 0 },
-  };
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 7, minutes: 0 },
+        end: { hours: 7, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
 
-  expect(isValidHourWindows([windowA, windowB])).toBe(true);
+  // triple
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 10, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+      {
+        start: { hours: 13, minutes: 0 },
+        end: { hours: 15, minutes: 0 },
+      },
+      {
+        start: { hours: 17, minutes: 0 },
+        end: { hours: 19, minutes: 0 },
+      },
+    ])
+  ).toBe(true);
 
-  const windowC: HourWindow = {
-    start: { hours: 11, minutes: 0 },
-    end: { hours: 15, minutes: 0 },
-  };
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 10, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+      {
+        start: { hours: 11, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+      {
+        start: { hours: 17, minutes: 0 },
+        end: { hours: 19, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
 
-  expect(isValidHourWindows([windowA, windowC])).toBe(false);
+  // all permutations of [
+  //   { hours: 0, minutes: 0 },
+  //   { hours: 6, minutes: 0 },
+  //   { hours: 12, minutes: 0 },
+  //   { hours: 18, minutes: 0 },
+  // ]
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 0, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+    ])
+  ).toBe(true);
 
-  const windowD: HourWindow = {
-    start: { hours: 22, minutes: 0 },
-    end: { hours: 1, minutes: 0 },
-  };
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 0, minutes: 0 },
+      },
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+    ])
+  ).toBe(true);
 
-  expect(isValidHourWindows([windowA, windowD])).toBe(true);
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+      {
+        start: { hours: 0, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+    ])
+  ).toBe(true);
 
-  const windowE: HourWindow = {
-    start: { hours: 21, minutes: 0 },
-    end: { hours: 23, minutes: 0 },
-  };
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 0, minutes: 0 },
+      },
+    ])
+  ).toBe(true);
 
-  expect(isValidHourWindows([windowD, windowE])).toBe(false);
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 0, minutes: 0 },
+      },
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
 
-  const windowF: HourWindow = {
-    start: { hours: 21, minutes: 0 },
-    end: { hours: 2, minutes: 0 },
-  };
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 0, minutes: 0 },
+      },
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
 
-  expect(isValidHourWindows([windowD, windowF])).toBe(false);
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 0, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
 
-  const windowG: HourWindow = {
-    start: { hours: 0, minutes: 30 },
-    end: { hours: 2, minutes: 0 },
-  };
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+      {
+        start: { hours: 0, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
 
-  expect(isValidHourWindows([windowD, windowG])).toBe(false);
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+      {
+        start: { hours: 0, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+      {
+        start: { hours: 0, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+      {
+        start: { hours: 0, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 0, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 0, minutes: 0 },
+      },
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 0, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 0, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 0, minutes: 0 },
+      },
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 0, minutes: 0 },
+      },
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 0, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+      {
+        start: { hours: 0, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 0, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 0, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 0, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 0, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 0, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  // all permutations of [
+  //   { hours: 23, minutes: 59 },
+  //   { hours: 6, minutes: 0 },
+  //   { hours: 12, minutes: 0 },
+  //   { hours: 18, minutes: 0 },
+  // ]
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 23, minutes: 59 },
+        end: { hours: 6, minutes: 0 },
+      },
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+    ])
+  ).toBe(true);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 23, minutes: 59 },
+      },
+    ])
+  ).toBe(true);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 23, minutes: 59 },
+      },
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+    ])
+  ).toBe(true);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+      {
+        start: { hours: 23, minutes: 59 },
+        end: { hours: 6, minutes: 0 },
+      },
+    ])
+  ).toBe(true);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 23, minutes: 59 },
+      },
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 23, minutes: 59 },
+      },
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 23, minutes: 59 },
+        end: { hours: 12, minutes: 0 },
+      },
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+      {
+        start: { hours: 23, minutes: 59 },
+        end: { hours: 18, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+      {
+        start: { hours: 23, minutes: 59 },
+        end: { hours: 18, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+      {
+        start: { hours: 23, minutes: 59 },
+        end: { hours: 12, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+      {
+        start: { hours: 23, minutes: 59 },
+        end: { hours: 12, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 23, minutes: 59 },
+        end: { hours: 18, minutes: 0 },
+      },
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 23, minutes: 59 },
+      },
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 23, minutes: 59 },
+        end: { hours: 6, minutes: 0 },
+      },
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 23, minutes: 59 },
+        end: { hours: 12, minutes: 0 },
+      },
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 23, minutes: 59 },
+      },
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 23, minutes: 59 },
+      },
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 23, minutes: 59 },
+        end: { hours: 18, minutes: 0 },
+      },
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+      {
+        start: { hours: 23, minutes: 59 },
+        end: { hours: 6, minutes: 0 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 12, minutes: 0 },
+      },
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 23, minutes: 59 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 23, minutes: 59 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 6, minutes: 0 },
+        end: { hours: 18, minutes: 0 },
+      },
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 23, minutes: 59 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 23, minutes: 59 },
+      },
+    ])
+  ).toBe(false);
+
+  expect(
+    isValidHourWindows([
+      {
+        start: { hours: 12, minutes: 0 },
+        end: { hours: 6, minutes: 0 },
+      },
+      {
+        start: { hours: 18, minutes: 0 },
+        end: { hours: 23, minutes: 59 },
+      },
+    ])
+  ).toBe(false);
 });
+
+function permute<Type>(permutation: Type[]): Type[][] {
+  const length = permutation.length,
+    result = [permutation.slice()],
+    c = new Array(length).fill(0);
+  let i = 1,
+    k,
+    p;
+
+  while (i < length) {
+    if (c[i] < i) {
+      k = i % 2 && c[i];
+      p = permutation[i];
+      permutation[i] = permutation[k];
+      permutation[k] = p;
+      ++c[i];
+      i = 1;
+      result.push(permutation.slice());
+    } else {
+      c[i] = 0;
+      ++i;
+    }
+  }
+  return result;
+}

1b432004517b7878a60680e81422696895a33094
BerkYeni <141870638+BerkYeni@users.noreply.github.com>
2023-09-27 14:44:35 +0300

    implementing create schedule form, implemented schedule name in the form, imported necessary shadcn components.

--- src/routes/CreateSchedule.tsx
+++ src/routes/CreateSchedule.tsx
@@ -1,29 +1,132 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+
 import { FC } from "react";
+import { Schedule } from "@/interfaces";
+import { Checkbox } from "@/components/ui/checkbox";
+
+// TODO: look into toast
+
+const week = [
+  { id: "monday", day: "Monday" },
+  { id: "tuesday", day: "Tuesday" },
+] as const;
+
+const FormSchema = z.object({
+  scheduleName: z.string().min(1, {
+    message: "Schedule Name must be at least 1 characters.",
+  }),
+  week: z.array(z.string()).refine((value) => value.some(Boolean)),
+});
 
 const CreateSchedule: FC = () => {
+  // export function CreateSchedule() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      scheduleName: "",
+      week: ["Monday", "Tuesday"],
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+    // toast({
+    //   title: "You submitted the following values:",
+    //   description: (
+    //     <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+    //       <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+    //     </pre>
+    //   ),
+    // });
+  }
+
   return (
     <div className="flex flex-col justify-center h-screen">
       <div className="p-2 mx-2 bg-zinc-200 rounded-md ">
-        <h1>Create a new schedule</h1>
-        <form action="">
-          <label className="block" htmlFor="name">
-            Name:
-          </label>
-          <input type="text" name="name" />
-
-          <label className="block" htmlFor="timeFrames">
-            Time Frames:
-          </label>
-          <input type="date" name="timeFrames" />
-
-          <div className="flex justify-end">
-            <input
-              className="block p-2 mt-5 bg-green-500 text-zinc-100 rounded-md font-bold"
-              type="submit"
-              value="Create Schedule"
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="scheduleName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Schedule Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Examples: Work, Study, Exercise..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-          </div>
-        </form>
+
+            <FormField
+              control={form.control}
+              name="week"
+              render={() => (
+                <FormItem>
+                  <div className="mb-4">
+                    <FormLabel className="text-base">Weekdays</FormLabel>
+                  </div>
+                  {week.map((item) => (
+                    <FormField
+                      key={item.id}
+                      control={form.control}
+                      name="week"
+                      render={({ field }) => {
+                        return (
+                          <FormItem
+                            key={item.id}
+                            className="flex flex-row items-start space-x-3 space-y-0"
+                          >
+                            <FormControl>
+                              <Checkbox
+                                checked={field.value?.includes(item.id)}
+                                onCheckedChange={(checked) => {
+                                  return checked
+                                    ? field.onChange([...field.value, item.id])
+                                    : field.onChange(
+                                        field.value?.filter(
+                                          (value) => value !== item.id
+                                        )
+                                      );
+                                }}
+                              />
+                            </FormControl>
+                            <FormLabel className="font-normal">
+                              {item.day}
+                            </FormLabel>
+                          </FormItem>
+                        );
+                      }}
+                    />
+                  ))}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex justify-end">
+              <Button type="submit">Create Schedule</Button>
+            </div>
+          </form>
+        </Form>
       </div>
     </div>
   );
